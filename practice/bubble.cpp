03680c6fcf5e6a1aa74a115de42ea9683f9ed1b1
mkjoseph <83678033+mkjoseph@users.noreply.github.com>
2021-11-12 15:03:51 -0800

    Create bubble.cpp

---
+++ practice/bubble.cpp
@@ -0,0 +1,16 @@
+// this is the bubble sort
+#include <iostream>
+
+
+int main()
+for(a=1; a<size; a++)
+    for(b=size-1; b>=a; b--) {
+        if (nums[b-1]) > nums[b]) { // if out of order
+        // exchange elements
+        t = nums[b-1];
+        nums[b-1] = numbs[b]
+        nums[b] = t
+
+        }
+        return 0;
+    }
